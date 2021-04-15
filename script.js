7feed3bdc6f8e84658f8ad03cd9f76ca4a6f098e
FernanEd <fernandoed.bm@gmail.com>
2021-04-15 18:01:03 -0500

    done

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
 const $button = document.querySelector("button");
+
 $button.addEventListener("click", (e) => {
-  let res = new XMLHttpRequest();
-  res.responseType = "json";
-  res.open(
-    "GET",
-    "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
-  );
-  res.send();
-  res.onload = () => {
-    let phrase = res.response;
+  let res = `{
+      "quoteText": "Let us be grateful to people who make us happy; they are the charming gardeners who make our souls blossom. ",
+      "quoteAuthor": "Marcel Proust",
+      "senderName": "",
+      "senderLink": "",
+      "quoteLink": "http://forismatic.com/en/3fe08c5b2e/"
+    }`;
 
-    phrase = {
-      quoteText:
-        "Let us be grateful to people who make us happy; they are the charming gardeners who make our souls blossom. ",
-      quoteAuthor: "Marcel Proust",
-      senderName: "",
-      senderLink: "",
-      quoteLink: "http://forismatic.com/en/3fe08c5b2e/",
-    };
+  let data = JSON.parse(res);
 
-    const $div = document.querySelector("#quote-div");
-
-    $div.innerHTML = `
+  const $div = document.querySelector("#quote-div");
+  $div.innerHTML = `
     <div class='quote'>
-      <p class='quote-text'>"${phrase.quoteText}"</p>
-      <p class='quote-author'>-${phrase.quoteAuthor}</p>
+      <p class='quote-text'>"${data.quoteText}"</p>
+      <p class='quote-author'>-${data.quoteAuthor}</p>
     </div>
     `;
-    console.log(phrase);
-  };
 });
